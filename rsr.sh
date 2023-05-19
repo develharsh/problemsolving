ef7551421736715f65bbfd01e2815e203fa581f2
develharsh <hvsreal.223@gmail.com>
2023-05-19 16:19:08 +0530

    modified script, divided solutions by folder

--- rsr.sh
+++ rsr.sh
@@ -1,10 +1,12 @@
+#v2
 if tsc -p tsconfig.json; then
     node index.js
-    if [ $1 != '.' ]; then
-        mkdir -p snippets/$1
-        cp index.ts snippets/$1/$2.ts
-    else
-        cp index.ts snippets/$2.ts
+    if [ $1 == 'c' ]; then
+        cp index.ts snippets/codeforces/$2.ts
+    elif [ $1 == 'l' ]; then
+        cp index.ts snippets/leetcode/$2.ts
+    elif [ $1 == 's' ]; then
+        cp index.ts snippets/standard-dsa/$2.ts
     fi
     if [ $3 == "y" ]; then
         cp template.ts index.ts
@@ -12,5 +14,22 @@ if tsc -p tsconfig.json; then
 else
     echo command returned some error
 fi
-rm *.js
-rm snippets/*.js
+find . -name "*.js" -type f -delete
+
+#v1
+# if tsc -p tsconfig.json; then
+#     node index.js
+#     if [ $1 != '.' ]; then
+#         mkdir -p snippets/$1
+#         cp index.ts snippets/$1/$2.ts
+#     else
+#         cp index.ts snippets/$2.ts
+#     fi
+#     if [ $3 == "y" ]; then
+#         cp template.ts index.ts
+#     fi
+# else
+#     echo command returned some error
+# fi
+# rm *.js
+# rm snippets/*.js
