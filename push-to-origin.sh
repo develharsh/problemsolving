5a0731d93e7b8bee1dcc405e3778c66b5e20972f
develharsh <hvsreal.223@gmail.com>
2023-06-04 02:42:06 +0530

    improved script

--- push-to-origin.sh
+++ push-to-origin.sh
@@ -5,4 +5,12 @@ git status
 sleep 3
 git commit -m "$1"
 sleep 3
+git push origin golang
+sleep 1
+git checkout master
+sleep 1
+git merge golang
+sleep 1
 git push origin master
+sleep 1
+git checkout golang
